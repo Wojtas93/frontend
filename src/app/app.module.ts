77d8a5dd40a3453531ac30692844da888de044bc
mdabrowski25 <m.dabrowski97@o2.pl>
2020-12-15 20:05:15 +0100

    Primary routing set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './home/home.component';
+import {HeaderComponent} from './header/header.component';
+import {ContactComponent} from './contact/contact.component';
+import {RouterModule, Routes} from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { HeaderComponent } from './header/header.component';
-import { ContactComponent } from './contact/contact.component';
+const routes: Routes = [{
+  component: HomeComponent,
+  path: 'home'
+}, {
+  path: '',
+  redirectTo: '/home',
+  pathMatch: 'full'
+}];
 
 @NgModule({
   declarations: [
@@ -14,9 +23,11 @@ import { ContactComponent } from './contact/contact.component';
     ContactComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
