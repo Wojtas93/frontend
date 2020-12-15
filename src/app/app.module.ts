4e3d08287e6901c0b8a160f434aeab91b365c8be
Wojciech.Bassara <wojciech.bassara@knowit.no>
2020-12-15 21:02:04 +0100

    booking added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { BookingComponent } from './booking/booking.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    BookingComponent
   ],
   imports: [
     BrowserModule
