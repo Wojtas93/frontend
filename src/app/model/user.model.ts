de7e2e02eece35b5c4f033837708b5e325694907
mdabrowski25 <m.dabrowski97@o2.pl>
2020-12-30 16:03:06 +0100

    input check

--- src/app/model/user.model.ts
+++ src/app/model/user.model.ts
@@ -3,11 +3,15 @@ export class User {
   username: string;
   password: string;
   role: string;
+  firstname: string;
+  lastname: string;
 
-  constructor(id: number, username: string, password: string, role: string) {
-    this.id = id;
+
+  constructor(username: string, password: string, role: string, firstname: string, lastname: string) {
     this.username = username;
     this.password = password;
     this.role = role;
+    this.firstname = firstname;
+    this.lastname = lastname;
   }
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {User} from '../model/user.model';
 
 @Component({
   selector: 'app-register',
@@ -6,13 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-  firstname: string;
-  username: string;
-  lastname: string;
-  password: string;
+  user: User = {
+    firstname: '',
+    lastname: '',
+    password: '',
+    role: '',
+    username: ''
+  };
   passwordConfirm: string;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
