b8a3e4036acf10b74f05fc4164ae6a41720fbc6a
mdabrowski25 <m.dabrowski97@o2.pl>
2021-01-04 13:21:22 +0100

    diverse routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,34 +4,14 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {HeaderComponent} from './header/header.component';
 import {ContactComponent} from './contact/contact.component';
-import {RouterModule, Routes} from '@angular/router';
 import {BookingComponent} from './booking/booking.component';
 import {RegisterComponent} from './register/register.component';
 import {LoginComponent} from './login/login.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {RoutingModule} from './routing/routing.module';
 
-const routes: Routes = [{
-  component: HomeComponent,
-  path: 'home'
-}, {
-  path: '',
-  redirectTo: '/home',
-  pathMatch: 'full'
-}, {
-  component: ContactComponent,
-  path: 'contact'
-}, {
-  component: BookingComponent,
-  path: 'booking'
-}, {
-  component: RegisterComponent,
-  path: 'register'
-}, {
-  component: LoginComponent,
-  path: 'login'
-}];
 
 @NgModule({
   declarations: [
@@ -45,10 +25,10 @@ const routes: Routes = [{
   ],
   imports: [
     BrowserModule,
-    RouterModule.forRoot(routes),
     FormsModule,
     HttpClientModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
