cec6be70b0dac2fd382a6a2e4020d6e7ebde0c66
Wojciech Bassara <wojciech.bassara@knowit.no>
2021-01-20 11:11:17 +0100

    connect booking.component.ts to back

--- src/app/booking/booking.component.ts
+++ src/app/booking/booking.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {Reservation} from '../model/reservation.model';
-import {User} from '../model/user.model';
 import {DatePipe} from '@angular/common';
+import {HotelBookingService} from '../services/hotel-booking.service';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-booking',
@@ -9,23 +10,43 @@ import {DatePipe} from '@angular/common';
   styleUrls: ['./booking.component.css']
 })
 export class BookingComponent implements OnInit {
-  reservation: Reservation = {
-    endDate: 0,
-    guest: new User('', '', '', '', '', ''),
-    isPaid: false,
-    room: '',
-    startDate: 0
-  };
+  reservationForm: FormGroup;
   datePipeString: string;
+  bookingForm: FormGroup;
+  errorBoolean: boolean;
 
-  constructor(private datePipe: DatePipe) {
+
+  constructor(private datePipe: DatePipe, private httpService: HotelBookingService) {
     this.datePipeString = datePipe.transform(Date.now(), 'yyyy-MM-dd');
   }
 
   ngOnInit(): void {
+    this.reservationForm = new FormGroup({
+        startDate: new FormControl(null, Validators.required),
+        endDate: new FormControl(null, Validators.required),
+        guest: new FormControl(null, Validators.required),
+        isPaid: new FormControl(null, Validators.required),
+        room: new FormControl(null, Validators.required),
+      }
+    );
   }
 
   onSubmit(): void {
-    alert('Booking add / not implemented yet');
+    const reservation: Reservation = {
+      startDate: this.reservationForm.value.startDate,
+      endDate: this.reservationForm.value.endDate,
+      guest: this.reservationForm.value.guest,
+      isPaid: this.reservationForm.value.isPaid,
+      room: this.reservationForm.value.room,
+    };
+
+    this.httpService.addReservation(reservation)
+      .subscribe((responseUser) => {
+          this.errorBoolean = false;
+        },
+        () => {
+          this.errorBoolean = true;
+          alert('Could make this reservation');
+        });
   }
 }
